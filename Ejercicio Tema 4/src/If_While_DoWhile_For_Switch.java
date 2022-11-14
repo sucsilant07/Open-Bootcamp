3cbe1bf81eb2b26359248dd4f884a23e46956f46
sucsilant07 <suc.programing07@gmail.com>
2022-11-14 11:34:42 -0500

    Practice exercises

---
+++ Ejercicio Tema 4/src/If_While_DoWhile_For_Switch.java
@@ -0,0 +1,67 @@
+public class If_While_DoWhile_For_Switch {
+    // EJERCICIO CON "IF"
+    //public static void main(String[] args) {
+    // int numeroIf = 0;
+
+    // if (numeroIf >0){
+    //     System.out.println("Es positivo");
+    // }
+    // else if (numeroIf <0){
+    //    System.out.println("Es negativo");
+    // }
+    // else {
+    //    System.out.println("Es igual a cero");
+    // }
+//-------------------------------------------------------
+    // EJERCICIO CON "WHILE"
+    //public static void main(String[] args) {
+    //   int numeroWhile = 0;
+
+    //   while (numeroWhile < 3){
+    //    System.out.println(numeroWhile);
+    //    numeroWhile++;
+    //}
+    //}
+//--------------------------------------------------------
+    // EJERCICIO CON "DO WHILE"
+    //public static void main(String[] args) {
+    //   int numeroWhile = 2;
+
+    //   do {
+    //       System.out.println(numeroWhile);
+    //       numeroWhile++;
+    //   }
+    //   while (numeroWhile < 3);
+    //}
+//--------------------------------------------------------
+    // EJERCICIO CON "FOR"
+    //public static void main(String[] args) {
+    //   int numeroFor[] = {0,1,2,3};
+
+    //   for (int i = 0; i <= 3; i++){
+    //       System.out.println(numeroFor[i]);
+    //   }
+    //}
+//--------------------------------------------------------
+    // EJERCICIO CON "SWITCH"
+    //public static void main(String[] args) {
+    //   var estacion = "Verano";
+
+    //   switch(estacion){
+    //       case "Primavera":
+    //           System.out.println("Es Primavera");
+    //           break;
+    //       case "Verano":
+    //           System.out.println("Es Verano");
+    //           break;
+    //       case "Invierno":
+    //           System.out.println("Es Invierno");
+    //           break;
+    //       case "Otoño":
+    //           System.out.println("Es Otoño");
+    //           break;
+    //       default:
+    //           System.out.println("No es una estacion");
+    //   }
+    //}
+}
